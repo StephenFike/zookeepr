87c4e072bd41571882672fcb6bc6b87bd296f1fb
Stephen Fike <sfike98@gmail.com>
2022-04-14 18:32:27 -0500

    added data middleman checking/ pushing to animal array

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -47,6 +54,50 @@ function findById(id, animalsArray) {
     return result;
 
 };
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    return animal;
+}
+
+function validateAnimal(animal){
+
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+
+    return true;
+}
+
+app.post('/api/animals', (req,res) => {
+    req.body.id = animals.length.toString();
+
+    if (!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.')
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
 
 app.get('/api/animals', (req, res) => {
 
